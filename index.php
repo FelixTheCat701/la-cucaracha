878f027c25c67d5d312054fae0fb97757e15a08d
Aquaman (Felix) <9010293@student.zadkine.nl>
2022-01-10 13:56:22 +0100

    kjjj

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
 ?>
 <link href="La-Cucaracha-syle.css" type="text/css" rel="stylesheet">
-<header>
-    <div class="container">
-        <h1 class="logo"></h1>
-    </div>
+<header
     <div class="navtop">
     <nav>
         <ul>
-            <li><a class="active" href="Index.php">Home</a></li>
+            <li><a class="active" href="index.php">Home</a></li>
             <li><a class="#" href="#">Over Mij</a></li>
             <li><a class="#" href="#">Opleiding</a></li>
             <li><a class="#" href="#">Hobby's</a></li>
