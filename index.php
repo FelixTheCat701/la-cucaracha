81a4284fe29f9ceba4b37f845beffdc12c5f79c9
Aquaman (Felix) <9010293@student.zadkine.nl>
2022-01-11 23:52:33 +0100

    poging tot titel maken
    het werkt niet helemaal maar uit eindelijk hopelijk wel

--- index.php
+++ index.php
@@ -6,16 +6,22 @@
     <nav>
         <ul>
             <li><a class="active" href="index.php">Home</a></li>
-            <li><a class="#" href="#">Over Mij</a></li>
-            <li><a class="#" href="#">Opleiding</a></li>
-            <li><a class="#" href="#">Hobby's</a></li>
-            <li><a class="#" href="#">Skills</a></li>
+            <li><a class="#" href="Over-ons.php">Over ons</a></li>
+            <li><a class="#" href="#">Jou auto</a></li>
+            <li><a class="#" href="#">Auto toevoegen</a></li>
+            <li><a class="#" href="#">Review</a></li>
             <li><a class="#" href="#">Werk Ervaring</a></li>
             <li><a class="#" href="#">Sociale Media</a></li>
             <li><a class="#" href="#">CV</a></li>
-            <li><a class="#" href="#">Contact</a></li>
+            <li><a class="#" href="Contact.php">Contact</a></li>
         </ul>
     </nav>
     </div>
 </header>
+<body>
+<div center>
+    <h1 class="titel">Home</h1>
+</div>
+
+</body>
 
