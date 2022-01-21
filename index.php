8518c7b7aeec050ac5809d57132eb18506c1d769
Aquaman (Felix) <9010293@student.zadkine.nl>
2022-01-21 08:54:21 +0100

    update
    update

--- index.php
+++ index.php
@@ -7,19 +7,19 @@
         <ul>
             <li><a class="active" href="index.php">Home</a></li>
             <li><a class="#" href="Over-ons.php">Over ons</a></li>
-            <li><a class="#" href="#">Jou auto</a></li>
+            <li><a class="#" href="#">Jouw auto</a></li>
             <li><a class="#" href="#">Auto toevoegen</a></li>
             <li><a class="#" href="#">Review</a></li>
-            <li><a class="#" href="#">Werk Ervaring</a></li>
+            <li><a class="#" href="#">Afspraken</a></li>
             <li><a class="#" href="#">Sociale Media</a></li>
-            <li><a class="#" href="#">CV</a></li>
+            <li><a class="#" href="#">geschiedenis </a></li>
             <li><a class="#" href="Contact.php">Contact</a></li>
         </ul>
     </nav>
     </div>
 </header>
 <body>
-<div center>
+<div class="center">
     <h1 class="titel">Home</h1>
 </div>
 
